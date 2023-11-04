94cc5e6e28c53a28d4932fd32941dbdb40deccd8
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2023-11-04 18:51:10 +0000

    File selection

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,3 +1,5 @@
+import com.sun.source.tree.TryTree;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -5,19 +7,38 @@ import java.util.ArrayList;
 
 public class MainApp
 {
-    public static void main(String[] args) throws FileNotFoundException
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner keyboard = new Scanner(System.in);
         ArrayList<Activity> list = new ArrayList<>();
 
-        /*Set<String> filesFound = (Set<String>) Stream.of(new File("Data").listFiles()).map(File::getName);
-        for (String name : filesFound)
+        for (final File fileEntry : new File("Data").listFiles())
         {
-            System.out.println(name);
-        }*/
+            System.out.println(fileEntry.getName());
+        }
 
-        String fileName = keyboard.nextLine();
-        File input = new File(fileName);
-        Scanner file = new Scanner(input);
+        boolean noFile = true;
+        File inputFile = null;
+
+        while (noFile)
+        {
+            String fileName = keyboard.nextLine();
+            try
+            {
+                inputFile = new File("Data/" + fileName);
+                noFile = false;
+            } catch (Exception e)
+            {
+                System.out.println("Incorrect file name");
+            }
+        }
+        Scanner file = new Scanner(inputFile);
+
+        if(file.hasNextLine())
+            file.nextLine();
+        while (file.hasNextLine)
+        {
+            String line = file.nextLine();
+
+        }
     }
 }
